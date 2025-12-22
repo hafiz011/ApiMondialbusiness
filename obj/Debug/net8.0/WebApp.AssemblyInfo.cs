0493bf7d674cd67239ef98625c98d625d5096c27
hafiz011 <hrhafij8@gmail.com>
2025-12-22 17:50:10 +0600

    update idea details page

--- obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f28a5bf99db7834de6ba8096df187321c8fc687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69859d65bb2b382c1c04d3719435bd1a736ff057")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
